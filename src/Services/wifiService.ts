c38ec815d3fafbc8066346e052fadd54a07b3a02
Carlos W T Machado <carloswtmachado@gmail.com>
2022-09-12 17:05:32 -0300

    feat: wifi routes

---
+++ src/Services/wifiService.ts
@@ -0,0 +1,44 @@
+import * as wifiRepository from '../Repositories/wifiRepository';
+import { CreateWifi } from '../Types/wifiType';
+import encrypter from '../Utils/encryptPasswordCRYPTR';
+
+export async function Create(wifi: CreateWifi){
+	const wifiFound = await wifiRepository.findByUserIdETitle(wifi.userId, wifi.title);
+	if(wifiFound !== null) throw {
+		type: 'Conflict',
+		message: 'user alredy have wifi with this title'
+	}
+
+	const encryptedPassword = encrypter(wifi.password);
+	await wifiRepository.create({...wifi, password: encryptedPassword});
+	return;
+}
+
+export async function GetAll(userId: number){
+	const wifis = await wifiRepository.findByUserId(userId);
+	if(wifis.length === 0) throw {
+		type: 'NotFound',
+		message: 'user has no wifis'
+	}
+	return wifis;
+}
+
+export async function GetById(wifiId: number, userId: number){
+	const wifi = await wifiRepository.findByIdEUserId(wifiId, userId);
+	if(wifi === null) throw {
+		type: 'NotFound',
+		message: 'Wifi not found'
+	}
+	return wifi;
+}
+
+export async function DeleteById(wifiId: number, userId: number){
+	const wifi = await wifiRepository.findByIdEUserId(wifiId, userId);
+	if(wifi === null) throw {
+		type: 'NotFound',
+		message: 'Wifi not found'
+	}
+
+	await wifiRepository.deleteById(wifiId);
+	return wifi;
+}
